fa88c30aba69f5ba47a86033f6ff620c849e6432
Lord-Giganticus <mrubins64@gmail.com>
2021-02-04 12:35:20 -0600

    Update both python scripts to reflect changes made in v2.5 folder

--- MP4toTHP.py
+++ MP4toTHP.py
@@ -1,102 +1,51 @@
-# Made by Lord-Giganticus
+import sys, os
 
-import os
-import shutil
-
-program_location = os.getcwd()
-
-os.mkdir('nonmp4')
-os.mkdir('thp_output')
-
-mp4_files = 0
-mp4_files2 = 0
+wav_file = bool(False)
 
 
-MP4_Location = input("Please input the folder where ONE mp4 video is:")
-os.chdir(MP4_Location)
-print(os.getcwd())
-input("If this is not the correct folder I swear...")
-
-def returnnonmp4():
-    os.chdir('nonmp4')
-    for file in os.listdir(os.getcwd()):
-        if file.endswith('.mp4') == False:
-            shutil.move(file, MP4_Location)
-    os.chdir(MP4_Location)
-    shutil.rmtree('nonmp4')
-for name in os.listdir(os.getcwd()):
-    if os.path.isfile(name) == True:
-        if name.endswith('.mp4') == False:
-            shutil.move(name, 'nonmp4')
-        elif name.endswith('.mp4') == True:
-            mp4_files += 1
-if mp4_files > 1:
-    input("There is more than one mp4 file in the folder! Please move it and then press enter.")
-    for file in os.listdir(os.getcwd()):
-        if os.path.isfile(file) == True:
-            if file.endswith('mp4') == True:
-                mp4_files2 += 1
-    if mp4_files2 > 1:
-        input("Well I gave you the chance to move it and ya didn't. Press enter to crash the program.")
-        exit()
-
-print("Not all THP videos have audio. Do you wish to have audio for yours? Then type the corresponding number to your choice.\n[1] Yes\n[2] No")
-Audio = input("")
-
-if Audio == "1":
-    wav_conv = open('wavconv.bat','w')
-    wav_conv.write('for %%a in ("*.mp4") do ffmpeg -i "%%a" "input.wav"')
-    wav_conv.close()
-
-    MP4_Conv = open('MP4toTHP.bat', 'w')
-    MP4_Conv.write('for %%a in ("*.mp4") do ffmpeg -i "%%a" "input.mp4"')
-    MP4_Conv.close()
-
-    os.system('cmd /c wavconv.bat')
-    os.system('cmd /c MP4toTHP.bat')
-    os.system('cmd /c ffmpeg -i input.mp4 -r 30 frame%03d.jpg')
-    os.system('cmd /c thpconv -j *.jpg -s input.wav -d thp_output/output.thp')
-    os.system('cmd /c del /f *.bat')
-    os.system('cmd /c del /f *.wav')
-    os.system('cmd /c del /f *.jpg')
-    os.system('cmd /c del /f input.mp4')
-    returnnonmp4()
-
-    print("Do you wish to rerun the program? If so type the corresponding number to your choice.\n [1] Yes\n [2] No")
-    rerun = input("")
-
-    if rerun == "1":
-        os.chdir(program_location)
-        os.system('cmd /c py -3 MP4toTHP.py')
-    elif rerun == "2":
-        exit()
-    else:
-        input("Whoops! That wasn't supposed to happen! Press enter to exit.")
-        exit()      
-elif Audio == "2":
-    MP4_Conv = open('MP4toTHP.bat', 'w')
-    MP4_Conv.write('for %%a in ("*.mp4") do ffmpeg -i "%%a" "input.mp4"')
-    MP4_Conv.close()
-
-    os.system('cmd /c MP4toTHP.bat')
-    os.system('cmd /c del /f *.bat')
-    os.system('cmd /c ffmpeg -i input.mp4 -r 30 frame%03d.jpg')
-    os.system('cmd /c thpconv -j *.jpg -d output.thp')
-    os.system('cmd /c del /f *.jpg')
-    os.system('cmd /c del /f input.mp4')
-    returnnonmp4()
-
-    print("Do you wish to rerun the program? If so type the corresponding number to your choice.\n [1] Yes\n [2] No")
-    rerun = input("")
-
-    if rerun == "1":
-        os.chdir(program_location)
-        os.system('cmd /c py -3 MP4toTHP.py')
-    elif rerun == "2":
-        exit()
-    else:
-        input("Whoops! That wasn't supposed to happen! Press enter to exit.")
-        exit()    
-else:
-    input("Whoops! That wasn't supposed to happen Press enter to exit.")
+if len(sys.argv) < 1:
+    print("There MUST be at least one argument!")
     exit()
+
+file = sys.argv[1]
+folder = os.path.dirname(file)
+name = os.path.splitext(file)
+name = name[0]
+if os.path.isfile(file) == False:
+    print("Argument is not a valid file.")
+    exit()
+else:
+    if file.endswith('.mp4') == False:
+        print("File is not a mp4 file.")
+        exit()
+os.chdir(folder)
+
+try:
+    os.system('THPConv')
+except:
+    if os.path.isfile('THPConv.exe') == False and os.path.isfile('dsptool.dll') == False:
+        os.system('cmd /c curl https://chaos-crew.000webhostapp.com/FastDL/THP.zip -o THP.zip')
+        os.system('cmd /c 7z x "THP.zip" -o"'+folder+'" -r')
+        os.remove('THP.zip')
+
+try:
+    sys.argv[2]
+    if sys.argv[2].endswith('-a') == True:
+        wav_file = bool(True)
+except:
+    pass
+
+if wav_file == True:
+    os.system('cmd /c ffmpeg -i "'+file+'" "input.wav"')
+
+os.system('cmd /c ffmpeg -i "'+file+'" -r 30 frame%03d.jpg')
+
+if os.path.isfile('input.wav') == False:
+    os.system('cmd /c thpconv -j *.jpg -d "'+name+'.thp"')
+else:
+    os.system('cmd /c thpconv -j *.jpg -s input.wav -d "'+name+'.thp"')
+    os.remove('input.wav')
+os.system('cmd /c del /f *.jpg')
+
+print("Complete.")
+exit()
--- THP2MP4.py
+++ THP2MP4.py
@@ -1,42 +1,22 @@
-import os
-import shutil
+import sys, os
 
-program_location = os.getcwd()
 
-THP_Location = input("Please input the folder of thp vids you wish to convert:")
-os.chdir(THP_Location)
-print(os.getcwd())
-input("If this is not the correct folder I swear...")
-os.mkdir('nonthp')
-os.mkdir('mp4output')
+if len(sys.argv) < 1:
+    print("There MUST be one argument!")
+    exit()
 
-for name in os.listdir('.'):
-    if os.path.isfile(name) == True:
-        if name.endswith('.thp') == False:
-            shutil.move(name, 'nonthp')
-
-THP_Conv = open('THPconv.bat', 'w')
-THP_Conv.write('for %%a in ("*.thp") do ffmpeg -i "%%a" "mp4output/%%~na.mp4"')
-THP_Conv.close()
-
-os.system('cmd /c THPconv.bat')
-os.system('cmd /c del /f *bat')
-os.chdir('nonthp')
-for file in os.listdir(os.getcwd()):
-    if os.path.isfile(file) == True:
-        if file.endswith('.thp') == False:
-            shutil.move(file, THP_Location)
-os.chdir(THP_Location)
-shutil.rmtree('nonthp')
-
-print("Do you wish to rerun this in another folder? If so, type the corresponding number to your choice. \n [1] Yes \n [2] No.")
-Rerun_Program = input()
-
-if Rerun_Program == "1":
-    os.chdir(program_location)
-    os.system('cmd /c py -3 THP2MP4.py')
-elif Rerun_Program == "2":
+file = sys.argv[1]
+folder = os.path.dirname(file)
+name = os.path.splitext(file)
+name = name[0]
+if os.path.isfile(file) == False:
+    print("Argument is not a valid file.")
     exit()
 else:
-    input("Whoops! That wasn't supposed to happen! Press enter to exit.")
-    exit()
+    if file.endswith('.thp') == False:
+        print("Argument is not a thp file.")
+        exit()
+os.chdir(folder)
+
+os.system('cmd /c ffmpeg -i "'+file+'" "'+name+'.mp4"')
+print("C")
