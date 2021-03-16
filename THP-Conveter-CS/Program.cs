502aeed0ded4379730cc3fea80cab8c24bd4326c
Lord-Giganticus <mrubins64@gmail.com>
2021-03-16 11:34:42 -0600

    change to gui because it'll be better

--- THP-Conveter-CS/Program.cs
+++ THP-Conveter-CS/Program.cs
@@ -1,14 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace THP_Converter_CS
+namespace THP_Conveter_CS
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
         }
     }
 }
